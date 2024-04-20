f5fbe1d14998cbeb9d422c76c3110f80ac67ab2b
Ghazman2024 <twaissighazi@gmail.com>
2024-04-20 10:13:33 +0300

    Initial commit

---
+++ Assets/Script/MenuButton.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButton : MonoBehaviour
+{
+    public void NewGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void QuitDesktop()
+    {
+        Application.Quit();
+    }
+}
